8084289c5a12332d246938cc49e1a7bf1a64e0fc
Benoît Prioux <benoit.prioux@gmail.com>
2017-07-24 11:34:56 +0200

    Include asciidoctor-diagram (#65)

--- src/test/resources/gems-provided/gems/asciidoctor-diagram-1.2.0-java/lib/asciidoctor-diagram/blockdiag.rb
+++
@@ -1,8 +0,0 @@
-require 'asciidoctor/extensions'
-require_relative 'version'
-
-Asciidoctor::Extensions.register do
-  require_relative 'blockdiag/extension'
-  block Asciidoctor::Diagram::BlockDiagBlock, :blockdiag
-  block_macro Asciidoctor::Diagram::BlockDiagBlockMacro, :blockdiag
-end
--- src/test/resources/gems-provided/gems/asciidoctor-diagram-1.2.0-java/lib/asciidoctor-diagram/util/java.rb
+++
@@ -1,5 +0,0 @@
-if RUBY_PLATFORM == "java"
-  require_relative 'java_jruby'
-else
-  require_relative 'java_rjb'
-end
